0b2c9092100a05d5da83d77f430e07bfca31b7fe
laena <lma64@mail.aub.edu>
2022-12-16 07:51:56 +0200

    11

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Form} from 'semantic-ui-react'
 import axios from 'axios'
+import '../create/Create.css'
 
 const Create = () => {
     const[itemName, setitemName] = useState('')
@@ -21,41 +21,51 @@ const Create = () => {
         })
     }
   return (
-    <Form>
-        <Form.Field>
-            <label>Name:</label>
-            <input 
-                name='name' 
-                placeholder='Item Name' 
-                onChange={(e) => setitemName(e.target.value)}
-            />
-        </Form.Field>
-        <Form.Field>
-            <label>Category Id:</label>
+    <div className='cont fs-4'>
+        <div className='row'>
+            <div className='col-6 mt-4'>
+                <label>Name:</label>
+                <br></br>
+                <input 
+                    name='name' 
+                    placeholder='Item Name' 
+                    onChange={(e) => setitemName(e.target.value)}
+                />
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-6 mt-4'>
+            <label>Category Id:</label><br></br>
             <input 
                 name='cat'
                 placeholder='Category ID'
                 onChange={(e) => setCat(e.target.value)}
             />
-        </Form.Field>
-        <Form.Field>
-            <label>Price:</label>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-6 mt-4'>
+            <label>Price:</label><br></br>
             <input 
                 name='price'
                 placeholder='Item Price'
                 onChange={(e) => setPrice(e.target.value)}
             />
-        </Form.Field>
-        <Form.Field>
-            <label>Quantity:</label>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-6 mt-4'>
+            <label>Quantity:</label><br></br>
             <input 
                 name='quantity'
                 placeholder='Item Quantity'
                 onChange={(e) => setQuant(e.target.value)}
             />
-        </Form.Field>
-        <Form.Field>
-            <label>Description</label>
+            </div>
+        </div>
+        <div className='row mt-4'>
+            <div className='col-6'>
+            <label>Description</label><br></br>
             <textarea 
                 name="des" 
                 rows="4" cols="50" 
@@ -64,9 +74,11 @@ const Create = () => {
             >
 
             </textarea>
-        </Form.Field>
-        <Button type='submit' onClick={sendDataToApi}>Submit</Button>
-    </Form>
+            </div>
+        </div>
+        <br></br>
+        <button className='btn btn-primary fs-4' type='submit' onClick={sendDataToApi}>Submit</button>
+    </div>
   )
 }
 
